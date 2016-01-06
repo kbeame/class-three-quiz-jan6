3fde0f766c291a71d1fdd5d95142fbb224bf5f44
Katherine Beame <katherine.beame@gmail.com>
2016-01-06 13:56:48 -0800

    Added counters but there is a typo

--- apps.js
+++ apps.js
@@ -12,50 +12,64 @@ var ready = prompt("Hello "+ userName + "! Are you ready to begin the KAT QUIZ? 
 
 //Begin Quiz//
 console.log(userName);
+var counter = 0;
 //Question One//
 var answer1 = prompt("Does Kat love pumpkins more than pumpkin spice lattes? Please answer Yes or No.");
   if (answer1.toUpperCase() === "YES" || answer1.toUpperCase() === "Y"
   || answer1.toUpperCase() === "YEA" || answer1.toUpperCase() === "YEAH")
   {
     alert("Correct " + userName + "! Pumpkins themselves are far superior in Kat\'s eyes.");
+    counter += 1;
+    alert("You now have " +counter + "/5 answers correct.");
     }
   else {
     alert("Bullocks " + userName + ". You should be ashamed of yourself.");
+    alert("You now have " +counter + "/5 answers correct.");
       }
 //Question Two//
 var answer2 = prompt("Is Kat a Canadian Citizen. Yes or No?");
   if (answer2.toUpperCase() === "YES" || answer2.toUpperCase() === "Y"
   || answer2.toUpperCase() === "YEA" || answer2.toUpperCase() === "YEAH") {
     alert("Right you are " + userName + "! Kat loves Nanimo Bars.");
+    counter += 1;
+    alert("You now have " +counter + "/5 answers correct.");
       }
   else {
     alert("HEY! " + userName + "! You shouldn\'t judge a book by its cover!");
+    alert("You now have " +counter + "/5 answers correct.");
       }
 //Question Three//
 var answer3 = prompt("Is Kat an American Citizen. Yes or No?");
   if (answer3.toUpperCase() === "YES" || answer3.toUpperCase() === "Y" ||
       answer3.toUpperCase() === "YEA" || answer3.toUpperCase() === "YEAH") {
     alert("Right you are " + userName + "! Kat loves eagles. Yum.");
+    counter += 1;
+    alert("You now have " +counter + "/5 answers correct.");
     }
   else {
     alert("..." + userName + ", please don\'t tell Donald Trump... JUST KIDDING! Trump can suck it. Kat\'s American.");
+    alert("You now have " +counter + "/5 answers correct.");
       }
 //Question Four//
 
 var answer4 = prompt("How many countries is Kat a citizen of?");
   if (parseInt(answer4) === 3 || answer4 === "3" || answer4.toUpperCase() === "THREE") {
     alert("Yes " + userName + "! USA, CANADA and UK!");
+    counter += 1;
+    alert("You now have " +counter + "/5 answers correct.");
     }
   else if (parseInt(answer4) < 3 || answer4 === "2" || answer4 === "1"
           || answer4.toUpperCase() === "ONE" || answer4.toUpperCase() === "TWO") {
     alert("Too low, " + userName + ". The correct answer is 3. USA, CANADA and UK!");
+    alert("You now have " +counter + "/5 answers correct.");
     }
-  else if (parseInt(answer4 > 3 || answer4 === "4" || answer4 === "5"
-          || answer4.toUpperCase() === "FOUR" || answer4.toUpperCase() === "FIVE") {
+  else if (parseInt(answer4) > 3) {
     alert("Too high, " + userName + ". The correct answer is 3. USA, CANADA and UK!");
+    alert("You now have " +counter + "/5 answers correct.");
     }
   else {
-    alert("Hm... I'm not sure you understood the question " + userName + ".")
+    alert("Hm... I'm not sure you understood the question " + userName + ". Anyway... moving on.")
+    alert("You now have " +counter + "/5 answers correct.");
   }
 
 }
